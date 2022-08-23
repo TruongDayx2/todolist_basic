978e055438d02d8402b397ac937a4051d42bdfef
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-08-24 00:21:40 +0700

    add job and push in local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
 
 
 function App() {
+
+
+  const [job,setJob] = useState('')
+  const [jobs,setJobs] = useState(() =>{
+  // lấy dữ liệu trên local storage
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs ?? []
+  })
+
+  const handleSubmit = () =>{
+    setJobs(prev => {
+      const newJobs = [...prev,job]
+      
+      // Save to local storage
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+
+      return newJobs
+    })
+    setJob('')
+  }
+
+
   return (
-    <div className="App">
+    <div style={{pading:32}}>
+      <input 
+        value={job}
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      
+      <ul>
+        {jobs.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
 
     </div>
   );
